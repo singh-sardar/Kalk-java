0e8ff87aa1d4b3ee1c256454717a25ae3ad24bee
singh-sardar <singh-sardar@github.com>
2018-06-15 16:37:52 +0200

    Removed hsl to rgb bug
    Removed hsl to rgb bug  and few comments

--- src/kalk/Colore.java
+++ src/kalk/Colore.java
@@ -1,80 +1,6 @@
 package kalk;
 
 public abstract class Colore {
-	/*
-	public abstract boolean uguale(Colore c);
-	public abstract boolean diverso(Colore c);
-	public abstract Colore somma(Colore c);
-	public abstract Colore differenza(Colore c);
-	public abstract Colore modula(Colore c);
-	public abstract double deltaE(Colore c);
-	public abstract Colore scala(double s);
-	public abstract Colore complementare();
-	public abstract Colore luminositaColore();
-	public abstract boolean coloreCaldo();
-	public abstract String schemaColore();
-
-
-
-
-	private static double abs(double d){
-		return d >=0 ? d : -d;
-	}
-	public static int[] hsl2rgb(double HSL[]){
-		int RGB[] = new int[3];
-	    double  RGB1[] = new double[3];
-	    HSL[1]/=100.0;
-	    HSL[2]/=100.0;
-	    if (HSL[1] == 0.0)
-	    {
-	        RGB[0] = RGB[1]= RGB[2] = (int) (HSL[2] * 255);
-	    }
-	    else
-	    {
-
-
-	        double chroma = (1 - abs(2 * HSL[2] - 1)) * HSL[1];
-	        double x = chroma * (1 - (abs(((HSL[0]/ 60)%2)-1) ));
-	        double m = HSL[2] - chroma/2;
-
-	        if(HSL[0]>=0.0&&HSL[0]<60.0){
-	            RGB1[0]=chroma;
-	            RGB1[1]=x;
-	            RGB1[2]=0;
-	        }
-	        if(HSL[0]>=60.0&&HSL[0]<120.0){
-	            RGB1[0]=x;
-	            RGB1[1]=chroma;
-	            RGB1[2]=0;
-	        }
-	        if(HSL[0]>=120.0&&HSL[0]<180.0){
-	            RGB1[0]=0;
-	            RGB1[1]=chroma;
-	            RGB1[2]=x;
-	        }
-	        if(HSL[0]>=180.0&&HSL[0]<240.0){
-	            RGB1[0]=0;
-	            RGB1[1]=x;
-	            RGB1[2]=chroma;
-	        }
-	        if(HSL[0]>=240.0&&HSL[0]<300.0){
-	            RGB1[0]=x;
-	            RGB1[1]=0;
-	            RGB1[2]=chroma;
-	        }
-	        if(HSL[0]>=300.0&&HSL[0]<360.0){
-	            RGB1[0]=chroma;
-	            RGB1[1]=0;
-	            RGB1[2]=x;
-	        }
-	        RGB[0]=(int)((RGB1[0]+m)*255);
-	        RGB[1]=(int)((RGB1[1]+m)*255);
-	        RGB[2]=(int)((RGB1[2]+m)*255);
-
-	    }
-
-		return RGB;
-	}*/
 
 	private int luminosita;//campo con valore tra 0 e 100 . se < 50 allora colore scuro
 
@@ -258,7 +184,6 @@ public abstract class Colore {
 			double chroma = (1 - abs(2 * HSL[2] - 1)) * HSL[1];
 			double x = chroma * (1 - (abs(((HSL[0]/ 60)%2)-1) ));
 			double m = HSL[2] - chroma/2;
-
 			if(HSL[0]>=0.0&&HSL[0]<60.0){
 				RGB1[0]=chroma;
 				RGB1[1]=x;
@@ -289,9 +214,9 @@ public abstract class Colore {
 				RGB1[1]=0;
 				RGB1[2]=x;
 			}
-			RGB[0]=(int)(RGB1[0]+m)*255;
-			RGB[1]=(int)(RGB1[1]+m)*255;
-			RGB[2]=(int)(RGB1[2]+m)*255;
+			RGB[0]=(int)((RGB1[0]+m)*255);
+			RGB[1]=(int)((RGB1[1]+m)*255);
+			RGB[2]=(int)((RGB1[2]+m)*255);
 
 		}
 
--- src/kalk/Use.java
+++ src/kalk/Use.java
@@ -52,49 +52,7 @@ public class Use {
 
 
 	}
-	static void coloriRgbUse(){/*
-		ColoreRgb c1 = new ColoreRgb(0,0,255,1);
-		ColoreRgb c2 = new ColoreRgb(234,154,105,1);
-		ColoreRgb c3 = new ColoreRgb();
-		System.out.println("Colore 1 : "+c1.schemaColore());
-		System.out.println("Colore 2 : "+c2.schemaColore());
-		System.out.println("Colore result : "+c3.schemaColore());;
-		c3=(ColoreRgb)c1.somma(c2);
-		System.out.println("Colore result Somma : "+c3.schemaColore());
-		c3=(ColoreRgb)c1.differenza(c2);
-		System.out.println("Colore result Differenza : "+c3.schemaColore());;
-		c3=(ColoreRgb)c1.modula(c2);
-		System.out.println("Colore result Modulazione : "+c3.schemaColore());;
-		System.out.println("Delta E Colori : "+Double.toString(c1.deltaE(c2)));;
-		System.out.println("Colore complementare Colore 1 : "+c1.complementare().schemaColore());
-		System.out.println("Luminosita Colore 1 : "+c1.luminositaColore().schemaColore());
-		if(((ColoreRgb)c1.luminositaColore()).getR()<130){
-			 System.out.println("E' un colore SCURO.\n Per cui ti conviene mettere il testo in sovraimpressione in BIANCO");
-		}else{
-			 System.out.println("E' un colore Chiaro.\n Per cui ti conviene mettere il testo in sovraimpressione in NERO");
-		}
-		
-		if(c1.coloreCaldo()){
-			 System.out.println(c1.schemaColore()+" E' un colore Caldo");
-		}else
-			 System.out.println(c1.schemaColore()+" E' un colore Freddo");
-		double lab[] = new double[3];
-		lab  = c1.rgb2lab();
-		System.out.println("Rappresentazione in lab: "+c1.schemaColore()+" ==> LAB("+Double.toString(lab[0])+','+Double.toString(lab[1])+','+Double.toString(lab[2])+')');
-		double hsl[] = new double[3];
-		hsl  = c1.rgb2hsl();
-		System.out.println("Rappresentazione in HSL: "+c1.schemaColore()+" ==> HSL("+Double.toString(hsl[0])+','+Double.toString(hsl[1])+','+Double.toString(hsl[2])+')');
-		System.out.println("Rappresentazione in HEX: "+c1.schemaColore()+" ==> HEX("+c1.rgb2hex()+')');
-		System.out.println("Colore 1 scalato di 0.5 "+c1.scala(0.5).schemaColore());
-		double HSL[] = new double[3];
-		HSL[0]=324;
-		HSL[1]=100;
-		HSL[2]=50;
-		int RGB[] = new int[3];
-		RGB = Colore.hsl2rgb(HSL);
-		System.out.println("HSL TO RGB HSL(324,100,50) ==> RGB("+Integer.toString(RGB[0])+','+Integer.toString(RGB[1])+','+Integer.toString(RGB[2])+",1)");
-	*/
-		}
+	
 	
 	static void coloriUse() {
 		Colore colRGB = new ColoreRgb(0,0,255);
@@ -131,7 +89,7 @@ public class Use {
 		System.out.println("Colore "+colHSL+" scalato di 0.5 "+colHSL.scala(0.5));
 		System.out.println("Scala grigio per colore "+colHSL+": "+colHSL.grayScale());
 		
-		/*
+		
 		double HSL[] = new double[3];
 		HSL[0]=324;
 		HSL[1]=100;
@@ -139,7 +97,7 @@ public class Use {
 		int RGB[] = new int[3];
 		RGB = Colore.hsl2rgb(HSL);
 		System.out.println("HSL TO RGB HSL(324,100,50) ==> RGB("+Integer.toString(RGB[0])+','+Integer.toString(RGB[1])+','+Integer.toString(RGB[2])+",1)");
-		*/
+		
 	}
 
 	
